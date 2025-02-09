9ea2aa3acf17b67d1405bb54d7b129876c011eb6
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2025-02-09 09:42:59 -0300

    test: criando roadmap dos testes do cap. 1

---
+++ __tests__/cap-1-refatoracao-primeiro-exemplo/cap-1-refatoracao-primeiro-exemplo.test.ts
@@ -0,0 +1,33 @@
+describe("Cap. 1 - Refatoração - Primeiro Exemplo", () => {
+   it.todo("Ponto de partida");
+
+   it.todo("Comentários sobre o programa inicial");
+
+   it.todo("Primeiro passo na refatoração");
+
+   it.todo("Decompondo a função statement");
+
+   it.todo("Removendo a variável play");
+
+   it.todo("Extraindo créditos por volume");
+
+   it.todo("Removendo a variável format");
+
+   it.todo("Status: muitas funções aninhadas");
+
+   it.todo("Separando as fases de calculo e de formatação");
+
+   it.todo("Status: separando em dois arquivos (e fases)");
+
+   it.todo("Reorganizando os calculos por tipo");
+
+   it.todo("Criando uma calculadora de apresentação");
+
+   it.todo("Passando funções para a calculadora");
+
+   it.todo("Deixando a calculadora de apresentação polimorfica");
+
+   it.todo("Status: criando os dados com a calculadora polimorfica");
+
+   it.todo("Considerações finais");
+});
