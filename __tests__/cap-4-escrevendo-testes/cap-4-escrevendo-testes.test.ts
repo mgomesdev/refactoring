3a34c8527deae9710f37077ee7c9ee186cc68ff2
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2025-05-04 11:05:59 -0300

    refactor: renomeia teste

---
+++ __tests__/cap-4-escrevendo-testes/cap-4-escrevendo-testes.test.ts
@@ -0,0 +1,14 @@
+describe("Aplicação simples que permite um usuario analisar e manipular um plano de produção", () => {
+   it.todo("O plano de produção tem uma demanda e o preço para cada província.");
+   it.todo(
+      "Cada provincia tem produtores, e cada um deles é capaz de produzir determinado número de unidades a um preço específico."
+   );
+   it.todo("A UI também mostra a receita que cada produtor teria se vendesse toda a sua produção.");
+   it.todo(
+      "A tela mostra o deficit (shotfall) na produção (a demanda menos a produção total) e o lucro (profit) para esseplano."
+   );
+   it.todo(
+      "A UI permite o usuário manipular a demanda e o preço, além da produção e o custo individual para ver o efeito no deficit de produção e nos lucros."
+   );
+   it.todo("Sempre que um usuario mudar qualquer numero na tela, todos os demais se atualizarão imediatamente.");
+});
