0c45c92954aed5774e9705f1a54f91dc5c7bd1ab
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2025-07-09 08:11:34 -0300

    feat: change function declaration

--- __tests__/cap-6-primeiro-conjunto-refatoracoes/153-change-function-declaration.test.ts
+++ __tests__/cap-6-primeiro-conjunto-refatoracoes/153-change-function-declaration.test.ts
@@ -1,1 +1,11 @@
-it("Change Function Declaration", () => {});
+import {
+   inNewEngland,
+   inNewEngandChange,
+   someCustomers,
+} from "../../src/cap-6-primeiro-conjunto-refatoracoes/153-change-function-declaration";
+it("Change Function Declaration", () => {
+   const newEnglanders = someCustomers.filter((c) => inNewEngland(c));
+   const newEnglandersChange = someCustomers.filter((c) => inNewEngandChange(c.address.state));
+   expect(newEnglanders).toEqual([{ address: { state: "MA" } }]);
+   expect(newEnglandersChange).toEqual([{ address: { state: "MA" } }]);
+});
--- src/cap-6-primeiro-conjunto-refatoracoes/153-change-function-declaration.ts
+++ src/cap-6-primeiro-conjunto-refatoracoes/153-change-function-declaration.ts
@@ -1,11 +1,45 @@
 export function circum(radius: number) {
-   return radius;
+   return circumference(radius);
 }
 
 export function circumference(radius: number) {
-   return radius;
+   return 2 * Math.PI * radius;
 }
 
-/*
- *
+/* XXX:
+ * em alguns casos, quando uma função possui um nome que não corresponde exatamente ao que se espera.
+ * precisamos atualizar essa função (nomes, parametros etc...) para que ela atenda a nossos requisitos.
+ * pode ser que ela esteja sendo utilizada em diversos locais separados, nesses casos.
+ * migre aos poucos, e sempre rode os testes para certificar de que o sistema continua funcionando corretamente.
+ * DICA: utilize o recurso de 'pesquisa' da sua IDE para buscar em todo o projeto os locais onde o método que será alterado está sendo utilizado.
+ */
+
+type StateCode = "MA" | "SP";
+
+type Customer = {
+   address: {
+      state: StateCode;
+   };
+};
+
+export function inNewEngland(customer: Customer) {
+   return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(customer.address.state);
+}
+
+export function inNewEngandChange(stateCode: StateCode) {
+   return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(stateCode);
+}
+
+export const someCustomers: Customer[] = [
+   {
+      address: { state: "MA" },
+   },
+];
+
+/* XXX:
+ * prefira especificar exatamente as propriedades que as funções irão utilizar.
+ * evite dependencias desnecessarias, por ex: 'inNewEngland' só utiliza a prop 'state' de customer.
+ * portanto, o parametro da função deverá ser apenas 'state', evitando dependencias desnecessarias (objeto customer inteiro).
+ * isso facilita a reutilização do código em diversos contextos diferentes.
+ * DICA: quando estiver refatorando, trabelhe em cima de copias da função original, e assim que tiver finalizado, substitua a antiga pela nova.
  */
